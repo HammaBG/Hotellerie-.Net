85d3419a3c7d2d5dd43e0b7727431138f77771a3
HammaBG <mohamedamine00879@gmail.com>
2024-04-27 16:30:47 +0100

    Ajoutez des fichiers projet.

---
+++ Hotellerie-X/Models/HotellerieModel/Appreciation.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Hotellerie_X.Models.HotellerieModel
+{
+    public class Appreciation
+    {
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "Nom Required !")]
+        [Display(Name = "Nom Personne")]
+        public string NomPers { get; set; }
+        [Required(ErrorMessage = "Comm Required !")]
+        [DataType(DataType.MultilineText)]
+        public string Commentaire { get; set; }
+        [ForeignKey("Hotel")]
+        public int HotelId { get; set; }
+        public Hotel? Hotel { get; set; } = null;
+    }
+}
---
+++ Hotellerie-X/Models/HotellerieModel/Hotel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hotellerie_X.Models.HotellerieModel
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Nom Required !")]
+        [StringLength(20,MinimumLength =3,ErrorMessage ="name length error")]
+        public string Name { get; set; }
+        [Range(1,5 ,ErrorMessage ="error range 1,5")]
+        public int Etoiles { get; set; }
+        [Required(ErrorMessage = "Ville Required !")]
+        public string Ville { get; set; }
+        [Required(ErrorMessage = "SiteWeb Required !")]
+        [Display(Name = "Site Web")]
+        [RegularExpression(@"^(http|https)://([\w-]+.)+[\w-]+(/[\w- ./?%&=])?$",
+            ErrorMessage = "The url must be in this form")]
+        public string SiteWeb { get; set; }
+        public string Tel { get; set; }
+
+
+        public virtual ICollection<Appreciation>? Appreciation { get; set; } =new  List<Appreciation>();
+    }
+}
